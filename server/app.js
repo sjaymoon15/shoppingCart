358f3214089a06d2f99ad155077a18ac35480506
sjaymoon15 <sjaymoon15@gmail.com>
2017-05-02 10:34:39 -0700

    add routes

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
+const routes = require('./routes/routes.js');
 
 const app = express();
 
 app.use(logger('dev'));
-app.use((req, res, next) => {
-  res.send('server works!');
-});
 
+routes(app);
+
+app.use((err, erq, res) => {
+  res.status(422).send({ error: err.message });
+})
 module.exports = app;
