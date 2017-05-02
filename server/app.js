4c92e3a7ae15469c1a56c695afc40138077fbf5b
sjaymoon15 <sjaymoon15@gmail.com>
2017-05-02 10:30:12 -0700

    add nodemon

--- server/app.js
+++ server/app.js
@@ -3,10 +3,9 @@ const logger = require('morgan');
 
 const app = express();
 
+app.use(logger('dev'));
 app.use((req, res, next) => {
   res.send('server works!');
 });
 
-app.listen(5227, () => {
-  console.log('Running on port 5227');
-});
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(5227, () => {
+  console.log('Running on port 5227');
+});
