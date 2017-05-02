6c2214594fa4711312bd4391cb3835819e849dfd
sjaymoon15 <sjaymoon15@gmail.com>
2017-05-02 12:54:46 -0700

    add cart controller

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,11 @@
 const ProductController = require('../controllers/product_controller');
+const CartController = require('../controllers/cart_controller');
 
 module.exports = (app) => {
   app.get('/api/products', ProductController.fetchProducts);
+  app.put('/api/products', ProductController.updateProduct);
+  app.get('/api/cart', CartController.getCartItems);
+  app.post('/api/cart', CartController.addItem);
+  app.delete('/api/cart/:id', CartController.deleteItem);
+  app.delete('/api/checkout', CartController.checkOutItems);
 };
