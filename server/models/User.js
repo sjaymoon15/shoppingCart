616193f936591e55a88b96fa206bb542bdc74ec9
sjaymoon15 <sjaymoon15@gmail.com>
2017-07-17 13:44:51 -0700

    before saving a model, bcrypt the password

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const UserSchema = new Schema({
   email: {
@@ -13,6 +14,20 @@ const UserSchema = new Schema({
     required: true,
   },
 });
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+        if (err) { return next(err); }
+
+        user.password = hash;
+        next();
+    });
+  });  
+})
 
 const User = mongoose.model('User', UserSchema);
 
